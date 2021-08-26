30fdd69c71d57341275993368f3614fb30033d9e
flakee <flakeexe@gmail.com>
2021-08-27 01:39:19 +0300

    fixing fileNotFound

--- tests/homework1/test_task3.py
+++ tests/homework1/test_task3.py
@@ -2,7 +2,7 @@ import os
 
 from homework1.task3 import find_maximum_and_minimum
 
-dir = os.getcwd() + '\\tests\\homework1\\test_file.txt'
+dir = os.getcwd() + '/tests/homework1/test_file.txt'
 
 
 def test_find_maximum_and_minimum_positive():
