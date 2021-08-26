8fc84c449df6e9f929e923a57580bd2618cd5918
flakee <flakeexe@gmail.com>
2021-08-27 01:34:40 +0300

    fixing fileNotFound

--- tests/homework1/test_task3.py
+++ tests/homework1/test_task3.py
@@ -1,7 +1,9 @@
+import os
 from homework1.task3 import find_maximum_and_minimum
+
+dir = os.getcwd() + '\\tests\\homework1\\test_file.txt'
 
 
 def test_find_maximum_and_minimum_positive():
-    assert find_maximum_and_minimum('C:\\Users\\flake\\projects\
-                                    \\homework-repository \\tests\
-                                    \\homework1\\test_file.txt') == (-123, 981)
+    print(dir)
+    assert find_maximum_and_minimum(dir) == (-123, 981)
