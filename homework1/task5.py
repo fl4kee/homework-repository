060bd6ca86858e9ee672d804f8f0039b9f17e748
Kirill Levedev <46884257+fl4kee@users.noreply.github.com>
2021-08-30 16:06:33 +0300

    Update task5.py

--- homework1/task5.py
+++ homework1/task5.py
@@ -1,6 +1,6 @@
 """
 Given a list of integers numbers "nums".
-You need to find a sub-array with length less equal to "", with maximal sum.
+You need to find a sub-array with length less equal to "k", with maximal sum.
 The written function should return the sum of this sub-array.
 Examples:
     nums = [1, 3, -1, -3, 5, 3, 6, 7], subarray_len = 3
