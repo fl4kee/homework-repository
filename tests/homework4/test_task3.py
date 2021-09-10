be78135ceae189eeee93ba5c6d994bbd4d87d793
flakee <flakeexe@gmail.com>
2021-09-10 17:29:02 +0300

    updated test_task3

--- tests/homework4/test_task3.py
+++ tests/homework4/test_task3.py
@@ -2,11 +2,15 @@ import pytest
 
 from homework4.task3 import my_precious_logger
 
-test_data = [({'string': 'error: error occured'}, {'message': 'error: error occured'}),
-             ({'string': 'No error'}, {'message': ''})]
+test_data = [({'string': 'error: error occured'}, {'stderr': 'error: error occured',
+                                                   'stdout': ''}),
+             ({'string': 'No error'}, {'stderr': '',
+                                       'stdout': 'No error'})]
 
 
 @pytest.mark.parametrize('test_data, expected', test_data)
 def test_my_precious_logger(capsys, test_data, expected):
     my_precious_logger(test_data['string'])
-    assert capsys.readouterr().err == expected['message']
+    out, err = capsys.readouterr()
+    assert out == expected['stdout']
+    assert err == expected['stderr']
