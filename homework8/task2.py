015b9791ec9ce29b433fcebc13fbdd943c0f5664
flakee <flakeexe@gmail.com>
2021-09-25 12:51:21 +0300

    removed file and added the task

--- homework8/task2.py
+++ homework8/task2.py
@@ -1,3 +1,22 @@
+# Write a wrapper class TableData for database table, that when initialized
+# with database name and table acts as collection object (implements Collection protocol).
+# Assume all data has unique values in 'name' column.
+# So, if presidents = TableData(database_name='example.sqlite', table_name='presidents')
+# then
+# len(presidents) will give current amount of rows in presidents table in database
+# presidents['Yeltsin'] should return single data row for president with name Yeltsin
+# 'Yeltsin' in presidents should return if president with same name exists in table
+# object implements iteration protocol. i.e. you could use it in for loops::
+# for president in presidents:
+# print(president['name'])
+# all above mentioned calls should reflect most recent data.
+# If data in table changed after you created collection instance,
+# your calls should return updated data.
+# Avoid reading entire table into memory. When iterating through records,
+# start reading the first record, then go to the next one, until records are exhausted.
+# When writing tests, it's not always neccessary to mock database calls completely.
+# Use supplied example.sqlite file as database fixture file.
+
 import sqlite3
 
 
