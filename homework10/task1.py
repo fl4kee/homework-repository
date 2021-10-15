77bf86d11364b23776e1d232265940af44d79433
flakee <flakeexe@gmail.com>
2021-10-15 17:42:02 +0300

    hw10

--- homework10/task1.py
+++ homework10/task1.py
@@ -1,28 +1,12 @@
 import asyncio
-import datetime
 import json
 import os
-import time
+from datetime import datetime
+from typing import Callable, List, Union
 
-import aiohttp
 import xmltodict  # type: ignore
+from aiohttp import ClientSession
 from bs4 import BeautifulSoup  # type: ignore
-
-t0 = time.time()
-BASE_URL = "https://markets.businessinsider.com"
-HEADERS = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)\
-                   Chrome/94.0.4606.61 Safari/537.36',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,\
-               */*;q=0.8,application/signed-exchange;v=b3;q=0.9'
-}
-
-
-current_date = datetime.datetime.now().strftime('%d.%m.%Y')
-# dict for currencies
-current_rate = {}
-# list for company data
-companies_data = []
 
 
 def round_float(float_num):
@@ -30,149 +14,246 @@ def round_float(float_num):
     return float(f"{float_num:.2f}")
 
 
-async def get_current_rate(session, id):
-    """
-    function takes an id that corresponds to certaing currency
-    list of ids you can find here http://www.cbr.ru/scripts/XML_val.asp?d=0
-    it returns current exchange rate in rubles
-    """
-    curr_rates_url = f'https://www.cbr.ru/scripts/XML_daily.asp?date_req={current_date}'
-    async with session.get(url=curr_rates_url, headers=HEADERS) as response:
-        response_text = await response.text()
-        currencies = xmltodict.parse(response_text)
-        for i in currencies['ValCurs']['Valute']:
-            if i['@ID'] == id:
-                current_rate['USD'] = float(i['Value'].replace(',', '.'))
-                break
+class WebScraper:
+    def __init__(self, current_date=datetime.now()):
+        self.current_date = current_date.strftime('%d.%m.%Y')
+        self.url = "https://markets.businessinsider.com"
+        self.headers = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)\
+                        Chrome/94.0.4606.61 Safari/537.36',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,\
+                    */*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+        }
+        self.number_of_pages = None
+        self.current_rate = None
+        self.parsed_data = []
+        if os.name == 'nt':
+            asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+        asyncio.run(self.gather_data())
 
+    async def get_current_rate(self, session: ClientSession, id: str) -> float:
+        """
+        function takes an id that corresponds to certaing currency
+        list of ids you can find here http://www.cbr.ru/scripts/XML_val.asp?d=0
+        it returns current exchange rate in rubles
+        returns 0 if id is not found
+        """
+        curr_rates_url = f'https://www.cbr.ru/scripts/XML_daily.asp?date_req={self.current_date}'
+        async with session.get(url=curr_rates_url) as response:
+            response_text = await response.text()
+            currencies = xmltodict.parse(response_text)
+            for i in currencies['ValCurs']['Valute']:
+                if i['@ID'] == id:
+                    return float(i['Value'].replace(',', '.'))
+            raise AttributeError('No id is found')
 
-async def parse_details_page(session, url):
-    """
-    function parses details page of company
-    return it s code, P/E and potential profit
-    potential profit is calcilated from week_high and week_low
-    if there no data for these variables potential profit cant be calculated
-    """
-    async with session.get(url=url, headers=HEADERS) as response:
-        response_text = await response.text()
-        soup = BeautifulSoup(response_text, 'lxml')
-        code = soup.find('span', class_='price-section__category').text.strip().split()[-1]
-        pe = float(soup.find('div', class_='snapshot__data-item')
-                       .find(text=True).strip().replace(',', ''))
-        try:
-            week_high = float(soup.find('div', class_='snapshot__data-item--right')
-                                  .find(text=True).strip().replace(',', ''))
-        except AttributeError:
-            week_high = None
-        try:
-            week_low = float(soup.find('div', class_='snapshot__data-item')
-                                 .find(text=True).strip().replace(',', ''))
-        except AttributeError:
-            week_low = None
-        if week_high and week_low:
-            potential_profit = round_float(week_high - week_low)
-        else:
-            potential_profit = 'No data for calculating potential profit'
-        return (code, pe, potential_profit)
+    async def fetch_details_page(self,
+                                 session: ClientSession,
+                                 url: str) -> tuple[str, Union[float, str], Union[str, float]]:
+        """
+        Takes url of details page and returns code, P\\E and potential profit in tuple
+        """
+        async with session.get(url) as response:
+            print(url)
+            response_text = await response.text()
+            decoded_response_text = response_text.encode('utf-8').decode('unicode-escape')
+            soup = BeautifulSoup(decoded_response_text, 'lxml')
+            company_code = self.get_company_code(soup)
+            company_pe = self.get_company_pe(soup)
+            company_potential_profit = self.get_company_potential_profit(soup)
+            return (company_code, company_pe, company_potential_profit)
 
+    async def fetch(self, session: ClientSession, url: str) -> None:
+        """
+        Takes url of a page and parses
+        and adds all information about one company in list with parsed data
+        """
+        async with session.get(url) as response:
+            print(url)
+            response_text = await response.text()
+            decoded_response_text = response_text.encode('utf-8').decode('unicode-escape')
+            company_items = await self.get_company_items(decoded_response_text)
+            for ci in company_items:
+                company_data = ci.find_all('td')
+                name = self.get_company_name(company_data)
+                price = self.get_company_price(company_data)
+                details_url = self.get_company_details_url(company_data)
+                growth = self.get_company_growth(company_data)
+                code, pe, potential_profit = await self.fetch_details_page(session, details_url)
 
-async def get_page_data(session, page, current_rate):
-    page_url = BASE_URL + f'/index/components/s&p_500?p={str(page)}'
-    # # async requests with  aiohttp
-    async with session.get(url=page_url, headers=HEADERS) as response:
-        response_text = await response.text()
-
-        # #  Getting data from soup object
-        soup = BeautifulSoup(response_text, 'lxml')
-        company_items = soup.find('tbody', class_="table__tbody").find_all("tr")
-        for ci in company_items:
-            company_data = ci.find_all('td')
-            details_page_link = BASE_URL + company_data[0].find('a').attrs['href']
-
-            company_name = company_data[0].find("a").text.strip()
-            curr_price = round_float(float(company_data[1].text.split()[0]
-                                                          .strip()
-                                                          .replace(',', '')) * current_rate)
-            code, pe, potential_profit = await parse_details_page(session, details_page_link)
-            growth = round_float(float(company_data[-1].find("span").text.strip().replace(',', '')))
-
-            companies_data.append(
-                {
+                self.parsed_data.append({
                     "code": code,
-                    "name": company_name,
-                    "price": curr_price,
+                    "name": name,
+                    "price": price,
                     "P/E": pe,
                     "growth": growth,
                     "potential-profit": potential_profit
-                }
-            )
+                })
 
-    print(f"[INFO] {page} page is parsed")
+    def get_company_potential_profit(self, data: BeautifulSoup) -> Union[str, float]:
+        """
+        function parses potential profit of company
+        if there is no information to parse returns coresponding message
+        """
+        try:
+            week_high: Union[float, None] = float(data.find("div", string="52 Week High")
+                                                      .parent
+                                                      .find(text=True)
+                                                      .strip()
+                                                      .replace(',', ''))
+        except AttributeError:
+            week_high = None
+        try:
+            week_low: Union[float, None] = float(data.find("div", string="52 Week Low")
+                                                     .parent
+                                                     .find(text=True)
+                                                     .strip()
+                                                     .replace(',', ''))
+        except AttributeError:
+            week_low = None
 
-    return companies_data
+        if week_high and week_low:
+            potential_profit = round_float((week_high - week_low) * self.current_rate)
+        else:
+            potential_profit = 'No data for calculating potential profit'
 
+        return potential_profit
 
-async def gather_data():
-    # creating session
-    async with aiohttp.ClientSession() as session:
+    def get_company_growth(self, data: BeautifulSoup) -> Union[float, str]:
+        """
+        function parses potential growth of company
+        if there is no information to parse returns coresponding message
+        """
+        try:
+            return round_float(float(data[-1].find("span").text.strip().replace(',', '')))
+        except AttributeError:
+            return 'No growth data for this company'
 
-        await get_current_rate(session, 'R01235')
-        # parsing pagination
-        response = await session.get(url=BASE_URL + '/index/components/s&p_500', headers=HEADERS)
-        soup = BeautifulSoup(await response.text(), 'lxml')
-        pages_count = int(soup.find("div", class_="finando_paging margin-top--small")
-                              .find_all("a")[-2].text)
+    def get_company_pe(self, data: BeautifulSoup) -> Union[float, str]:
+        """
+        function parses P\\E of company
+        if there is no information to parse returns coresponding message
+        """
+        try:
+            return round_float(float(data.find("div", string="P/E Ratio").parent
+                                                                         .find(text=True)
+                                                                         .strip()
+                                                                         .replace(',', '')))
+        except AttributeError:
+            return 'No P/E data for this company'
 
-        # task queue
+    def get_company_code(self, data: BeautifulSoup) -> str:
+        """
+        function parses code of company
+        if there is no information to parse returns coresponding message
+        """
+        try:
+            return data.find('span', class_='price-section__category').text.strip().split()[-1]
+        except AttributeError:
+            return 'No code data for this company'
+
+    def get_company_name(self, data: BeautifulSoup) -> str:
+        """
+        function parses name of company
+        if there is no information to parse returns coresponding message
+        """
+        try:
+            return data[0].find("a").text.strip()
+        except AttributeError:
+            return 'No name data for this company'
+
+    def get_company_details_url(self, data: BeautifulSoup) -> str:
+        """
+        function parses url of company
+        if there is no information to parse returns coresponding message
+        """
+        try:
+            return self.url + data[0].find('a').attrs['href']
+        except AttributeError:
+            return 'No URL for this company'
+
+    async def get_company_items(self, text: str) -> BeautifulSoup:
+        """
+        function parses all company data fro row in a table
+        if there is no information to parse returns coresponding message
+        """
+        try:
+            soup = BeautifulSoup(text, 'lxml')
+            return soup.find('tbody', class_="table__tbody").find_all("tr")
+        except AttributeError as e:
+            return f'No companies found, {e}'
+
+    async def get_number_of_pages(self, session: ClientSession, url: str) -> int:
+        """
+        function parses and returns number of pages
+        """
+        async with session.get(url) as response:
+            text = await response.text()
+            decoded_text = text.encode('utf-8').decode('unicode-escape')
+            soup = BeautifulSoup(decoded_text, 'lxml')
+            return int(soup.find("div", class_="finando_paging margin-top--small")
+                           .find_all("a")[-2].text)
+
+    def get_company_price(self, data: BeautifulSoup) -> Union[float, str]:
+        """
+        function parses price of company
+        if there is no information to parse returns coresponding message
+        """
+        try:
+            return round_float(float(data[1].text.split()[0]
+                                            .strip()
+                                            .replace(',', '')) * self.current_rate)
+        except AttributeError:
+            return 'No price for this company'
+
+    async def gather_data(self) -> None:
+        """
+        Creating and delegating tasks
+        """
         tasks = []
+        async with ClientSession(headers=self.headers) as session:
+            self.current_rate = await self.get_current_rate(session, 'R01235')
+            index_url = self.url + '/index/components/s&p_500'
+            self.number_of_pages = await self.get_number_of_pages(session, url=index_url)
 
-        # scrapper works for every page
-        for page in range(1, pages_count + 1):
-            task = asyncio.create_task(get_page_data(session, page, current_rate['USD']))
-            tasks.append(task)
+            for page in range(1, self.number_of_pages + 1):
+                url = self.url + f'/index/components/s&p_500?p={str(page)}'
+                task = asyncio.create_task(self.fetch(session, url))
+                tasks.append(task)
 
-        await asyncio.gather(*tasks)
-
-
-def most_expensv_cmpns(data):
-    """filteres data by most expensive price"""
-    return sorted(data, key=lambda x: x['price'], reverse=True)[0:10]
-
-
-def lowest_pe(data):
-    """filteres data by lowest P/E"""
-    return sorted(data, key=lambda x: x['P/E'])[0:10]
+            await asyncio.gather(*tasks)
 
 
-def highest_growth(data):
-    """filteres data by highest growth"""
-    return sorted(data, key=lambda x: x['growth'], reverse=True)[0:10]
+class Data:
+    def __init__(self, data: List) -> None:
+        self.data = data
 
+    def most_expensv_cmpns(self, data: List) -> List:
+        """filteres data by most expensive price"""
+        return sorted(data, key=lambda x: x['price'], reverse=True)[0:10]
 
-def highest_profit(data):
-    """filteres data by highest potential-profit"""
-    filtered_data = list(filter(lambda x: isinstance(x['potential-profit'], float), data))
-    return sorted(filtered_data, key=lambda x: x['potential-profit'], reverse=True)[0:10]
+    def lowest_pe(self, data: List) -> List:
+        """filteres data by lowest P/E"""
+        filtered_data = list(filter(lambda x: isinstance(x['P/E'], float), data))
+        return sorted(filtered_data, key=lambda x: x['P/E'])[0:10]
 
+    def highest_growth(self, data: List) -> List:
+        """filteres data by highest growth"""
+        return sorted(data, key=lambda x: x['growth'], reverse=True)[0:10]
 
-def create_file(name, filter_func):
-    """Creates file with filtered data"""
-    with open(name, "w", encoding="utf-8") as file:
-        json.dump(filter_func(companies_data), file, indent=4, ensure_ascii=False)
+    def highest_profit(self, data: List) -> List:
+        """filteres data by highest potential-profit"""
+        filtered_data = list(filter(lambda x: isinstance(x['potential-profit'], float), data))
+        return sorted(filtered_data, key=lambda x: x['potential-profit'], reverse=True)[0:10]
 
+    def create_file(self, name: str, filter_func: Callable) -> None:
+        """Creates file with filtered data"""
+        with open(name, "w", encoding="utf-8") as file:
+            json.dump(filter_func(self.data), file, indent=4, ensure_ascii=False)
 
-def main():
-    # if os is windows set corresponding event loop policy
-    if os.name == 'nt':
-        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    asyncio.run(gather_data())
-    # creating json files
-    create_file(f'10_most_expensive_stocks_{current_date}.json', most_expensv_cmpns)
-    create_file(f'10_lowest_p_e_{current_date}.json', lowest_pe)
-    create_file(f'10_highest_growth{current_date}.json', highest_growth)
-    create_file(f'10_highest_profit{current_date}.json', highest_profit)
-
-
-if __name__ == '__main__':
-    main()
-    print(time.time() - t0)
+    def create_files(self) -> None:
+        """creating files with data"""
+        self.create_file('10_most_expensive_stocks_.json', self.most_expensv_cmpns)
+        self.create_file('10_lowest_p_e_.json', self.lowest_pe)
+        self.create_file('10_highest_growth.json', self.highest_growth)
+        self.create_file('10_highest_profit.json', self.highest_profit)
--- tests/homework10/test_task1.py
+++ tests/homework10/test_task1.py
@@ -1,8 +1,85 @@
-# from homework10.task1_classes import WebScraper, Data
-# from datetime import datetime
+import os
+from datetime import datetime
 
-# def test_scraper_output():
-#     t0 = datetime.now()
-#     data = WebScraper().parsed_data
-#     Data(data).create_files()
-#     print(datetime.now() - t0)
+import pytest
+from aioresponses import aioresponses  # type: ignore
+
+from homework10.task1 import Data, WebScraper
+
+HEADERS = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)\
+                   Chrome/94.0.4606.61 Safari/537.36',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,\
+               */*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Charset': 'utf-8'
+}
+current_date = datetime(2021, 9, 10, 17, 5, 55)
+
+print("HEY HEY", os.path.realpath(__file__))
+
+with open(os.path.join(os.path.dirname(__file__), 'test_index_page.html'), 'r') as f:
+    fake_index_html = ''.join(f.readlines())
+
+with open(os.path.join(os.path.dirname(__file__), 'abt-stock.html'), 'r') as f:
+    fake_abt_html = ''.join(f.readlines())
+
+with open(os.path.join(os.path.dirname(__file__), 'aos-stock.html'), 'r') as f:
+    fake_aos_html = ''.join(f.readlines())
+
+with open(os.path.join(os.path.dirname(__file__), 'mmm-stock.html'), 'r') as f:
+    fake_mmm_html = ''.join(f.readlines())
+
+with open(os.path.join(os.path.dirname(__file__), '10_highest_growth_test.json'), 'r') as f:
+    test_10_highest_growth = f.readlines()
+
+with open(os.path.join(os.path.dirname(__file__), '10_highest_profit_test.json'), 'r') as f:
+    test_10_highest_profit = f.readlines()
+
+with open(os.path.join(os.path.dirname(__file__), '10_lowest_p_e_test.json'), 'r') as f:
+    test_10_10_lowest_p_e = f.readlines()
+
+with open(os.path.join(os.path.dirname(__file__), '10_most_expensive_stocks_test.json'), 'r') as f:
+    test_10_most_expensive_stocks = f.readlines()
+
+print(test_10_highest_growth)
+
+
+@pytest.fixture
+def mock_aioresponse():
+    with aioresponses() as m:
+        yield m
+
+
+@pytest.mark.asyncio
+def test_webscraper(mock_aioresponse, mocker):
+    mocker.patch("homework10.task1.WebScraper.get_current_rate", return_value=200)
+    mock_aioresponse.get('https://markets.businessinsider.com/index/components/s&p_500',
+                         headers=HEADERS, payload=fake_index_html)
+    mock_aioresponse.get('https://markets.businessinsider.com/index/components/s&p_500?p=1',
+                         headers=HEADERS, payload=fake_index_html)
+    mock_aioresponse.get('https://markets.businessinsider.com/abt',
+                         headers=HEADERS, payload=fake_abt_html)
+    mock_aioresponse.get('https://markets.businessinsider.com/aos',
+                         headers=HEADERS, payload=fake_aos_html)
+    mock_aioresponse.get('https://markets.businessinsider.com/mmm',
+                         headers=HEADERS, payload=fake_mmm_html)
+
+    data = WebScraper(current_date).parsed_data
+    Data(data).create_files()
+
+    with open(os.path.join(os.path.dirname(__file__), '../../10_highest_growth.json')) as f:
+        assert test_10_highest_growth == f.readlines()
+
+    with open(os.path.join(os.path.dirname(__file__), '../../10_highest_profit.json')) as f:
+        assert test_10_highest_profit == f.readlines()
+
+    with open(os.path.join(os.path.dirname(__file__), '../../10_lowest_p_e_.json')) as f:
+        assert test_10_10_lowest_p_e == f.readlines()
+
+    with open(os.path.join(os.path.dirname(__file__), '../../10_most_expensive_stocks_.json')) as f:
+        assert test_10_most_expensive_stocks == f.readlines()
+
+    os.remove('10_highest_growth.json')
+    os.remove('10_highest_profit.json')
+    os.remove('10_lowest_p_e_.json')
+    os.remove('10_most_expensive_stocks_.json')
