452c099df81c2a10d9bccb91f08ff1ef5cd97bf4
flakee <flakeexe@gmail.com>
2021-10-16 17:15:11 +0300

    hw11

---
+++ tests/homework11/test_task2.py
@@ -0,0 +1,14 @@
+from homework11.task2 import Order
+
+
+def test_order_strategy():
+    def morning_discount(order):
+        return order - order * 0.25
+
+    def elder_discount(order):
+        return order - order * 0.90
+
+    order_1 = Order(100, morning_discount)
+    assert order_1.final_price() == 75
+    order_2 = Order(100, elder_discount)
+    assert order_2.final_price() == 10
